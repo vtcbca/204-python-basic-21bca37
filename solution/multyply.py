0ed4c74a48a86b3000b8f91d480e63e846df3e6b
21bca37 <96609373+21bca37@users.noreply.github.com>
2022-03-30 09:55:44 +0530

    Add files via upload

---
+++ solution/multyply.py
@@ -0,0 +1,7 @@
+#was to enter two number and print multipication using function
+def multiply():
+    a=input("Enter 1st value:")
+    b=input("Enter 2nd value:")
+    c=a*b
+    print("multipication of two number is ".format(c))
+multiply()
---
+++ solution/peligdrom.py
@@ -0,0 +1,11 @@
+a=int(input("Enter any number:"))
+no=a
+sum=0
+while(a>0):
+    r=a%10
+    sum=sum*10+r
+    a=a//10
+if(sum==no):
+    print("{} is a palindrom number".format(no))
+else:
+    print("{} is not a palindrom number".format(no))
